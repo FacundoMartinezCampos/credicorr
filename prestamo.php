d3ff190d2ad40e8efff24936e76cfcd40b3f5d5b
AlejandroFabianCampos <alejandrofabiancampos2000@gmail.com>
2020-02-02 15:23:12 -0300

    Fix

---
+++ prestamo.php
@@ -0,0 +1,186 @@
+<!doctype html>
+<html lang="es">
+  <head>
+    <title>Credicorr$ &mdash;Préstamos personales </title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">    
+    
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/jquery-ui.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="css/jquery.fancybox.min.css">
+
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+
+    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+
+    <link rel="stylesheet" href="css/aos.css">
+    <link rel="shortcut icon" type="image/png" href="images/favicon.png">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Bebas+Neue&display=swap" rel="stylesheet">
+    
+  </head>
+  <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+  
+
+
+  <div class="site-wrap">
+
+    <div class="site-mobile-menu site-navbar-target">
+      <div class="site-mobile-menu-header">
+        <div class="site-mobile-menu-close mt-3">
+          <span class="icon-close2 js-menu-toggle"></span>
+        </div>
+      </div>
+      <div class="site-mobile-menu-body"></div>
+    </div>
+   
+    
+    <header class="site-navbar js-sticky-header site-navbar-target" role="banner">
+
+      <div class="container">
+        <div class="row align-items-center">
+          
+          <div class="col-6 col-xl-2">
+            <h1 class="mb-0 site-logo"><a href="index.html" class="h2 mb-0 credicorrr">Credicorr$ </a></h1>
+          </div>
+
+          <div class="col-12 col-md-10 d-none d-xl-block">
+            <nav class="site-navigation position-relative text-right" role="navigation">
+
+              <ul class="site-menu main-menu js-clone-nav mr-auto d-none d-lg-block">
+                <li><a href="index.html#faq-section" class="nav-link">Preguntas</a></li>
+                
+                
+                
+                
+               
+                <li><a href="" class="nav-link">Solicitá tu préstamo</a></li>
+                <li class="social"><a href="#contact-section" class="nav-link"><span class="icon-facebook"></span></a></li>
+              </ul>
+            </nav>
+          </div>
+
+
+          <div class="col-6 d-inline-block d-xl-none ml-md-0 py-3" style="position: relative; top: 3px;"><a href="#" class="site-menu-toggle js-menu-toggle float-right"><span class="icon-menu h3"></span></a></div>
+
+        </div>
+      </div>
+      
+    </header>
+    <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+      <div class="headerprestamo">
+        <div class="container">
+          <div class="row align-items-center justify-content-center">
+            <h3 class="paraquien fondoblanco">Préstamos personales para:</h3>
+            <div class="col-md-12">
+              <p><ul class="quienes">
+                <li>Jubilados (ANSES)</li>
+                <li>Empleados públicos (Banco Provincia)</li>
+                <li>Empleados privados</li>
+                <li>Prefectura, Gendarmería y la Policía de la Ciudad de Buenos Aires</li>
+                <li>Personal Civil y Militar del Ejército, Fuerza Aérea y Armada</li>
+              </ul></p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+         
+ 
+        <section class="formulario">
+        <div class="centrar">
+
+      <div class="bg10">
+        <div class="row">
+        <form action="#"  method="post">
+          <input type="text" class="cont" id="nombre" name="nombre" placeholder="Nombre" required><span class="fa  user"></span> <br/>
+          
+          <input type="text" class="cont" id="apellido" name="apellido" placeholder="Apellido" required><span class="fa  user"></span> <br/>
+          
+          <input type="text" id="email" class="cont" name="email" placeholder="Email" required>
+          <span class="fa "></span>
+          <br/>
+          
+          <input type="number" id="celular" class="cont" name="celular" placeholder="Celular" required> <span class="fa fa telefono"></span> <br/>
+          
+          <input type="text" id="provincia" class="cont" name="provincia" placeholder="Provincia" required> <span class="fa fa provincia"></span> <br/>
+          
+          <input type="text" id="ciudad" class="cont" name="ciudad" placeholder="Ciudad" required> <span class="fa fa ciudad"></span> <br/>
+          
+          <input type="number" id="dni" class="cont" name="dni" placeholder="DNI" required> <span class="fa fa dni"></span> <br/>
+          
+          <input type="text" id="trabajo" class="cont" name="trabajo" placeholder="¿Dónde trabajas?" required> <span class="fa fa trabajo"></span> <br/>
+          
+          <input type="number" id="monto" class="cont" name="monto" placeholder="Monto deseado: Hasta $70.000" required> <span class="fa fa monto"></span> <br/>
+          
+          <input type="number" id="cuotas" class="cont" name="cuotas" placeholder="Hasta 24 cuotas fijas" required> <span class="fa fa cuotas"></span> <br/>
+
+          
+          
+          <textarea rows="10" cols="40" id="boxing"  class="cont" placeholder="Mensaje"></textarea> <span class="fa  comment"></span><br/>
+          
+          
+          <div class="botonformulario2">
+          <input type="submit"  value="SOLICITAR" id="submit_button">
+        </div>
+        </form>
+       </div>
+      </div>
+    </div>
+  </section>
+
+
+
+
+
+        
+        
+        
+        
+        
+        <footer class="site-footer">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-12">
+                <div class="row">
+                  <div class="col-md-12 textofooter">
+                    <p>El otorgamiento del préstamo personal, se encuentra sujeto a calificación crediticia y condiciones de contratación. Las condiciones de los préstamos personales variarán de acuerdo a la calificación crediticia del interesado y las condiciones de mercado y políticas vigentes al momento del otorgamiento. Monto máximo $ 70.000, mínimo $ 3.000. Plazo: máximo 18 meses, mínimo 3 meses. Tasa máxima que incluye costo financiero total ANUAL para ARGENTINA (CFT). (132%). Ej. Para un préstamo de 2000 en 3 meses: Tasa Nominal Anual fija (78.8%), Costo financiero total ANUAL (132%). Ejemplo: Para un préstamo de $ 3000,00 en tres meses abona 3 cuotas iguales y fijas de $1300, y para seis meses 6 cuotas iguales y fijas de $ 800,00. La tasa será decreciente para los clientes con antigüedad y buen comportamiento de pago. Pre-cancelación: Todos los préstamos son pre cancelables abonando el interés correspondiente a la fecha del efectivo pago con más el saldo de la cuenta capital. Renovación: Abonando el 50% del préstamo podrá renovarse el mismo con la posibilidad, sujeta a aprobación, de aumentar el monto de otorgamiento. Los préstamos están disponibles para empleados en relación de dependencia. Los pagos podrán realizarse por débito automático, o en sucursales Pago Fácil, Rapipago, Cobro Express, Provincia Pagos, Ripsa Pagos y otras entidades. Para el caso de mora en el pago de las cuotas la Empresa se reserva el derecho de iniciar las acciones legales extrajudiciales y judiciales pertinentes.</p>
+                  </div>
+                </div>
+              </div>
+                
+                  </form>
+                </div>
+              </div>
+          </footer>
+      
+        </div> <!-- .site-wrap -->
+      
+        <script src="js/jquery-3.3.1.min.js"></script>
+        <script src="js/jquery-ui.js"></script>
+        <script src="js/popper.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/owl.carousel.min.js"></script>
+        <script src="js/jquery.countdown.min.js"></script>
+        <script src="js/jquery.easing.1.3.js"></script>
+        <script src="js/aos.js"></script>
+        <script src="js/jquery.fancybox.min.js"></script>
+        <script src="js/jquery.sticky.js"></script>
+        <script src="js/isotope.pkgd.min.js"></script>
+        <script src="js/faq.js"></script>
+     
+        <script src="js/script.js"></script>
+        
+        <script src="js/main.js"></script>
+      
+        
+        </body>
