6f127906aa84a4c9f0bd1e7d58ce652ff42b03ea
Facundo <54776310+FacundoMartinezCampos@users.noreply.github.com>
2020-02-02 18:56:25 -0300

    no anda xd

--- post-form.php
+++ post-form.php
@@ -1,16 +1,10 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-echo 'asd';
 // using SendGrid's PHP Library
 // https://github.com/sendgrid/sendgrid-php
 // require 'vendor/autoload.php'; // If you're using Composer (recommended)
 // Comment out the above line if not using Composer
 require("./sendgrid-php.php");
 // If not using Composer, uncomment the above line
-echo 'sendgrid api key ->' . getenv('SENDGRID_API_KEY');
 $email = new \SendGrid\Mail\Mail(); 
 $email->setFrom("solo-formularios@credicorr.com.ar", "Credicorr System");
 $email->setSubject("Formulario Credicorr");
@@ -19,7 +13,6 @@ $email->addContent("text/plain", "and easy to do anywhere, even with PHP");
 $email->addContent(
     "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
 );
-echo 'sendgrid api key ->' . getenv('SENDGRID_API_KEY');
 $sendgrid = new \SendGrid('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');
 try {
     echo 'This is also executing';
@@ -30,3 +23,5 @@ try {
 } catch (Exception $e) {
     echo 'Caught exception: '. $e->getMessage() ."\n";
 }
+
+?>
