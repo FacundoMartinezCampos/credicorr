bfc31258e46e9241a3da9ea610abcfe023ecda44
AlejandroFabianCampos <alejandrofabiancampos2000@gmail.com>
2020-02-02 15:29:52 -0300

    Added sendgrid

--- post-form.php
+++ post-form.php
@@ -1,11 +1,28 @@
 <?php
 
 echo 'asd';
-
-if ($_SERVER['REQUEST_URI'] === 'https://credicorr.com.ar/post-form' && $_SERVER['REQUEST_METHOD'] === 'POST') {
-    echo 'Hola mundo';
-}
-
-if ($_SERVER['REQUEST_URI'] === 'https://credicorr.com.ar/post-form.php' && $_SERVER['REQUEST_METHOD'] === 'POST') {
-    echo 'Hola mundo';
+// using SendGrid's PHP Library
+// https://github.com/sendgrid/sendgrid-php
+// require 'vendor/autoload.php'; // If you're using Composer (recommended)
+// Comment out the above line if not using Composer
+require("./sendgrid-php.php");
+// If not using Composer, uncomment the above line
+$email = new \SendGrid\Mail\Mail();
+$email->setFrom("solo-formularios@credicorr.com.ar", "Credicorr");
+$email->setSubject("Formulario Credicorr.");
+$email->addTo("testing446688@gmail.com", "Example User");
+$email->addContent(
+    "text/plain", "and easy to do anywhere, even with PHP"
+);
+$email->addContent(
+    "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
+);
+$sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
+try {
+    $response = $sendgrid->send($email);
+    print $response->statusCode() . "\n";
+    print_r($response->headers());
+    print $response->body() . "\n";
+} catch (Exception $e) {
+    echo 'Caught exception: ',  $e->getMessage(), "\n";
 }
