f2aa4a5c16ce827372d337cc4a3141f660305ae6
AlejandroFabianCampos <alejandrofabiancampos2000@gmail.com>
2020-02-02 15:06:39 -0300

    Fix

--- post-form.php
+++ post-form.php
@@ -3,3 +3,7 @@
 if ($_SERVER['REQUEST_URI'] === 'https://credicorr.com.ar/post-form' && $_SERVER['REQUEST_METHOD'] === 'POST') {
     echo 'Hola mundo';
 }
+
+if ($_SERVER['REQUEST_URI'] === 'https://credicorr.com.ar/post-form.php' && $_SERVER['REQUEST_METHOD'] === 'POST') {
+    echo 'Hola mundo';
+}
